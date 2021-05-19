4c81a2ff022aabc152e1702d463b818dad1630bc
Manuel Sonnleithner <manuel.sonnleithner@agenatrader.com>
2021-05-19 15:52:29 +0200

    AboutUs page setup

---
+++ react-portfolio/src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// Page component
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <AboutSection></AboutSection>
+        </div>
+    )
+}
+
+export default AboutUs
