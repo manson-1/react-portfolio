c7df0925e6818be56f2ce95392a48ce283084b8a
Manuel Sonnleithner <manuel.sonnleithner@agenatrader.com>
2021-05-24 17:37:06 +0200

    ServicesSection + FaqSection

--- react-portfolio/src/pages/AboutUs.js
+++ react-portfolio/src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from 'react'
 // Page component
 import AboutSection from '../components/AboutSection';
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
     return (
         <div>
             <AboutSection></AboutSection>
+            <ServicesSection></ServicesSection>
+            <FaqSection></FaqSection>
         </div>
     )
 }
