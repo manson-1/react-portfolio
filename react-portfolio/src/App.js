a86027583198520c867e40f0b138a421cbca03a1
Manuel Sonnleithner <manuel.sonnleithner@agenatrader.com>
2021-05-24 18:09:31 +0200

    AboutSection Styling

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,11 @@
 // import pages
 import AboutUs from './pages/AboutUs';
+import GlobalStyle from "./components/GlobalStyle"
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle></GlobalStyle>
       <AboutUs></AboutUs>
     </div>
   );
--- react-portfolio/src/components/AboutSection.js
+++ react-portfolio/src/components/AboutSection.js
@@ -1,34 +1,68 @@
 import React from "react";
-import home1 from '../img/home1.png'
+import home1 from "../img/home1.png";
+// styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAboutDiv>
+      <StyledDescriptionDiv>
         <div className="title">
-          <div className="hide">
+          <StyledHideDiv>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyledHideDiv>
+          <StyledHideDiv>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHideDiv>
+          <StyledHideDiv>
             <h2>true.</h2>
-          </div>
+          </StyledHideDiv>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
-          <img src={home1} alt="guy with camera" />
-      </div>
-    </div>
+      </StyledDescriptionDiv>
+      <StyledImageDiv>
+        <img src={home1} alt="guy with camera" />
+      </StyledImageDiv>
+    </StyledAboutDiv>
   );
 };
+
+// styled components
+const StyledAboutDiv = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescriptionDiv = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImageDiv = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const StyledHideDiv = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
